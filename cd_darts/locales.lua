8e816ba4f11de6b81d186eca89d5b2f9961302fd
Rampodious the 2nd <ramp.clash@gmail.com>
2023-01-10 13:49:35 +0000

    darts - locales

---
+++ cd_darts/locales.lua
@@ -0,0 +1,11 @@
+Locales = { 
+    ['EN'] = { --english
+        ['command_startgame'] = 'Start a darts game',
+        ['command_giveup'] = 'Give up in your current darts game',
+        ['command_createboard'] = 'Create and place a dartboard',
+        ['command_deleteboard'] = 'Delete your current dartboard',
+        ['item_missing'] = 'You are missing the required dart board item',
+        ['you'] = 'You',
+        ['opponent'] = 'Opponent',
+    },
+}
