a88b53161e6d65bb924f8b850a29fadedadf0073
Rampodious the 2nd <ramp.clash@gmail.com>
2023-01-10 13:49:28 +0000

    armwrestling - locales

---
+++ cd_armwrestling/locales.lua
@@ -0,0 +1,11 @@
+Locales = { 
+    ['EN'] = { --english
+        ['join_game'] = 'Press [E] to arm wrestle',
+        ['spam_to_win'] = 'Spam [E] to win',
+        ['wait_or_leave'] = 'Wait for another player or press [X] to leave',
+        ['game_full'] = 'No more players at this time',
+        ['command_spawn_table'] = 'Spawn an arm wrestling table',
+        ['command_delete_table'] = 'Delete the closest arm wrestling table',
+        ['table_not_found'] = 'No arm wrestling table found',
+    },
+}
---
+++ cd_armwrestling/locales_ui.js
@@ -0,0 +1,10 @@
+const LOCALES = {
+    "win": "YOU WIN",
+    "lose": "YOU LOSE",
+
+    "go": "GO",
+
+    "instructions_start":"Press <kbd>E</kbd> to arm wrestle",
+    "instructions_play":["Spam", "<kbd>E</kbd>", "to win"],
+    "instructions_leave":"Wait for another player or press <kbd>X</kbd> to leave"
+}
