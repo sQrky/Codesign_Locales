97de8b7ed096abf0c4b4f635ff58bd75046aeedf
Rampodious the 2nd <ramp.clash@gmail.com>
2023-01-10 13:49:44 +0000

    gunrange - locales

---
+++ cd_gunrange/locales.lua
@@ -0,0 +1,9 @@
+Locales = { 
+    ['EN'] = { --english
+        ['highScore'] = 'You are number %s on the leaderboard for %s!',
+        ['command_movetarget'] = 'Open the gunrange movement UI',
+        ['command_practice'] = 'Practice shooting at the gunrange',
+        ['command_start'] = 'Compete for a highscore in the gunrange',
+        ['command_scoreboard'] =' Open the Gunrange topscore'
+    },
+}
---
+++ cd_gunrange/locales_ui.js
@@ -0,0 +1,15 @@
+const LOCALES = {
+    "ar":"Assault Rifles",
+    "hg":"Handguns",
+    "lmg":"LMG",
+    "sg":"Shotguns",
+    "smg":"SMG",
+    "sn":"Snipers",
+    "sw":"Special",
+
+    "points":"Points",
+    "stop":"STOP",
+
+    "yourScore":"Your score is:",
+    "readyText":"Get Ready",
+}
