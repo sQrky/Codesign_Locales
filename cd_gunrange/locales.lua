2348b13de665d210bc634766d972d638f7d33265
Nick Van Damme <van.damme.nick@telenet.be>
2023-01-10 15:00:13 +0100

    gunrange dutch locales

--- cd_gunrange/locales.lua
+++ cd_gunrange/locales.lua
@@ -4,6 +4,13 @@ Locales = {
         ['command_movetarget'] = 'Open the gunrange movement UI',
         ['command_practice'] = 'Practice shooting at the gunrange',
         ['command_start'] = 'Compete for a highscore in the gunrange',
-        ['command_scoreboard'] =' Open the Gunrange topscore'
+        ['command_scoreboard'] ='Open the Gunrange topscore'
     },
+    ['NL'] = { --english
+    ['highScore'] = 'Je bent nummer %s in de rangschikking voor %s!',
+    ['command_movetarget'] = 'Open de UI om doelwitten te verplaatsen',
+    ['command_practice'] = 'Oefen met schieten op de schietbaan',
+    ['command_start'] = 'Doe mee aan de ranglijst op de schietbaan',
+    ['command_scoreboard'] ='Open de schietbaan rangschikking'
+},
 }
