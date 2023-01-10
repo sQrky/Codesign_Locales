a61b3c6c116037fdd3a4225084902a518dd044de
Nick Van Damme <van.damme.nick@telenet.be>
2023-01-10 14:56:22 +0100

    arm wrestling
    Dutch locales

---
+++ cd_armwrestling/locales_ui_nl.js
@@ -0,0 +1,10 @@
+const LOCALES = {
+    "win": "WINNAAR",
+    "lose": "VERLIEZER",
+
+    "go": "START",
+
+    "instructions_start":"Druk <kbd>E</kbd> om te arm worstelen",
+    "instructions_play":["Spam", "<kbd>E</kbd>", "om te winnen"],
+    "instructions_leave":"Wacht op een andere speler of druk <kbd>X</kbd> om te stoppen"
+}
