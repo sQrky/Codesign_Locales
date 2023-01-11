0a0452fac897d1816020464592693ee699ebd615
Rampodious the 2nd <ramp.clash@gmail.com>
2023-01-11 20:23:49 +0000

    vehicle control

--- cd_vehiclecontrol/locales.lua
+++ cd_vehiclecontrol/locales.lua
@@ -1,6 +1,9 @@
 Locales = { 
     ['EN'] = { --english
+        ['chat_description'] = 'Open the vehicle control UI',
         ['not_in_vehicle'] = 'You are not in a vehicle',
         ['blacklisted_vehclass'] = 'This type of vehicle can not use the vehicle control UI',
+        ['livery_distance_check'] = 'You can only change liveries at set locations. A waypoint has been set on your map',
+        ['extras_distance_check'] = 'You can only change extras at set locations. A waypoint has been set on your map',
     },
 }
