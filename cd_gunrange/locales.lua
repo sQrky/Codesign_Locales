81a987adfab34eb07ecedaa06b8f102c4cda6b5a
Rampodious the 2nd <ramp.clash@gmail.com>
2023-02-19 23:16:36 +0000

    gunrange - locales github sync

--- cd_gunrange/locales.lua
+++ cd_gunrange/locales.lua
@@ -6,11 +6,12 @@ Locales = {
         ['command_start'] = 'Compete for a highscore in the gunrange',
         ['command_scoreboard'] ='Open the Gunrange topscore'
     },
-    ['NL'] = { --english
-    ['highScore'] = 'Je bent nummer %s in de rangschikking voor %s!',
-    ['command_movetarget'] = 'Open de UI om doelwitten te verplaatsen',
-    ['command_practice'] = 'Oefen met schieten op de schietbaan',
-    ['command_start'] = 'Doe mee aan de ranglijst op de schietbaan',
-    ['command_scoreboard'] ='Open de schietbaan rangschikking'
-},
+
+    ['NL'] = { --dutch
+        ['highScore'] = 'Je bent nummer %s in de rangschikking voor %s!',
+        ['command_movetarget'] = 'Open de UI om doelwitten te verplaatsen',
+        ['command_practice'] = 'Oefen met schieten op de schietbaan',
+        ['command_start'] = 'Doe mee aan de ranglijst op de schietbaan',
+        ['command_scoreboard'] ='Open de schietbaan rangschikking'
+    },
 }
