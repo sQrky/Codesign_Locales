a14863d9e12c38dfb289c513cfc12d754bcff01e
Rampodious the 2nd <ramp.clash@gmail.com>
2023-02-19 23:16:11 +0000

    spawnselect - locales github sync

--- cd_spawnselect/locales_ui.js
+++ cd_spawnselect/locales_ui.js
@@ -1,6 +1,7 @@
 const LOCALES = {
-  "LAST_POSITION":"Last Pos",
-  "LIST_OF_SPAWNS":"List of spawn locations",
+    "LAST_POSITION":"Last Pos",
+    "LIST_OF_SPAWNS":"List of spawn locations",
 
-  "SPAWN":"Spawn"
+    "SPAWN":"Spawn",
+    "RESET_SCALE":"Reset Scale"
 }
