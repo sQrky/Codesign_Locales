34f60e4f848f8888a597f7528630d86d9d0a997b
Ramp <ramp.clash@gmail.com>
2023-06-11 23:34:08 +0100

    cctv - DE locales thanks to JeyCraft

--- cd_cctv/locales.lua
+++ cd_cctv/locales.lua
@@ -6,9 +6,28 @@ Locales = {
         ['command_placecctv'] = 'Place a cctv camera',
         ["disabled"] = "Camera is disabled",
         ["too_far"] = "You are trying to place the camera too far from your distance",
+        ["cannot_place_vehicles"] = "You cannot place cctv's on vehicles",
         ["intrustion_detected"] = "Intrusion Detected on your CCTV Camera %s",
         ["camera_disabled_stop_viewing"] = "Camera has been disabled so the viewing has stopped",
         ["camera_removed_stop_viewing"] = "Camera has been removed so the viewing has stopped",
-        ["no_item"] = "Looks like you are missing the item %s"
+        ["no_item"] = "Looks like you are missing the item %s",
+        ["loading_data"] = "Wait until the data is loaded", -- added for people using discord perms, because it takes time to compare the discord roles. Only shown to people using discord perms.
+        ["data_loaded"] = "Data is loaded, opening UI" -- added for people using discord perms, because it takes time to compare the discord roles. Only shown to people using discord perms.
     },
+
+    ['DE'] = { --german
+        ['player_not_found'] = 'Die von Ihnen eingegebene Spieler-ID ist in der Stadt nicht verfügbar.',
+        ['cannot_add_self'] = 'Du kannst dich nicht zur Kamera hinzufügen',
+        ['command_opencctvui'] = 'Öffne das Kamera Nutzerinterface',
+        ['command_placecctv'] = 'Platziere eine Überwachungskamera',
+        ["disabled"] = "Kamera ist deaktiviert",
+        ["too_far"] = "Du versuchst, die Kamera zu weit von dir entfernt zu platzieren.",
+        ["cannot_place_vehicles"] = "Du kannst keine kameras auf Fahrzeugen Platzieren.",
+        ["intrustion_detected"] = "Bewegung auf Ihrer Überwachungs-Kamera erkannt %s",
+        ["camera_disabled_stop_viewing"] = "Die Kamera wurde deaktiviert, so dass die Anzeige gestoppt wurde.",
+        ["camera_removed_stop_viewing"] = "Die Kamera wurde entfernt, so dass die Anzeige gestoppt wurde.",
+        ["no_item"] = "Sieht so aus als fehlt dir das nötige item, %s",
+        ["loading_data"] = "Warten, bis die Daten geladen sind", -- added for people using discord perms, because it takes time to compare the discord roles. Only shown to people using discord perms.
+        ["data_loaded"] = "Daten werden geladen, UI wird geöffnet" -- added for people using discord perms, because it takes time to compare the discord roles. Only shown to people using discord perms.
+    }, 
 }
